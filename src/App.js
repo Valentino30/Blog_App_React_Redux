daddaa8a6969dead3715a356e8905759b3a6a976
Valentino Giudice <giudicevalentino@gmail.com>
2019-06-05 19:32:23 +0200

    redux_crash_course_with_react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import PostForm from './components/PostForm';
+import Posts from './components/Posts';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className='App'>
+        <PostForm />
+        <Posts />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
